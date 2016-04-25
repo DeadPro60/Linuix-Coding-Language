e5a91f841ba8caeb1de0afb37a779ad497c9bd87
DeadPro60 <cjlegoben10@gmail.com>
2016-04-24 18:30:55 -0700

    adding in the source code

---
+++ Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	/**
+	 * Linuix Code Compiler
+	 * Made by DeadPro60
+	 */
+	
+	public static void main(String[] args) { //Start compiler
+		CPU.LoadProgram("src/res/Program"); //Compile This...
+	}
+	
+}
