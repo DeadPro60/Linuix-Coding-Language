a12fc4f05f28d89d2c863626f8e5cc232768e035
DeadPro60 <cjlegoben10@gmail.com>
2016-04-24 18:34:09 -0700

    Create cls.java

---
+++ src/compiler/commands/cls.java
@@ -0,0 +1,13 @@
+package compiler.commands;
+
+public class cls {
+
+	public static void Execute(String[] args) {
+		Proceed(args);
+	}
+	public static void Proceed(String[] args) {
+			for(int i = 0; i < 1000; i++) {
+				System.out.println();
+			}
+		}
+}
